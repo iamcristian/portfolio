1807bebdd517791db50a08a656ffb541988f0dd7
cristian <crisarandosyse@gmail.com>
2023-08-25 21:07:53 -0400

    Add responsive to navbar menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
 import { GiWorld } from "react-icons/gi";
+import {AiOutlineClose} from "react-icons/ai"
+
 import { ReactComponent as SunIcon } from "../../assets/icons-theme/sun-icon.svg";
 import { ReactComponent as MoonIcon } from "../../assets/icons-theme/moon-icon.svg";
 
-import { useContext } from "react";
+import { BiMenu } from "react-icons/bi";
+
+import { useContext, useState } from "react";
 import { GlobalContext } from "../../context/GlobalStateContext";
 
-import { header } from "../../data/common.json"
+import { header } from "../../data/common.json";
 
-import './Header.css'
+import "./Header.css";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
 
   const { theme, setTheme, language, setLanguage } = useContext(GlobalContext);
 
@@ -30,8 +35,8 @@ const Header = () => {
           <GiWorld className={`logo-nav ${theme}`} />
         </Link>
 
-        <nav className="navbar">
-          <ul id="nav">
+        <nav className={`navbar`}>
+          <ul id="nav" className={openMenu?"active":""}>
             <li className="inav">
               <Link className="links" to="/">
                 {header.home[language]}
@@ -71,6 +76,24 @@ const Header = () => {
           <span className="language" onClick={handleLanguageChange}>
             {language}
           </span>
+        </div>
+
+        <div className="menu_mobile">
+          {openMenu ? (
+            <AiOutlineClose
+              className={`nav-icon`}
+              onClick={() => {
+                setOpenMenu(!openMenu);
+              }}
+            />
+          ) : (
+            <BiMenu
+              className={`nav-icon`}
+              onClick={() => {
+                setOpenMenu(!openMenu);
+              }}
+            />
+          )}
         </div>
       </div>
     </header>
